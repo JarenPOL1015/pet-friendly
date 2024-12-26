b071613a4c330b6396737c1ba287da9a0e96bcc4
JarenPOL1015 <jarapazm@espol.edu.ec>
2024-12-26 17:10:58 -0500

    Nosotros LISTO (falta imgs de uds)

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,14 +1,44 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardContent,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonCardSubtitle
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+    IonCardContent,
+    IonGrid,
+    IonRow,
+    IonCol,
+    IonCardSubtitle
+]
 })
 export class Tab2Page {
+
+  creadores = [
+    { imagen: 'assets/jaren.jpg', nombre: 'Jaren Pazmiño' },
+    { imagen: 'assets/jaren.jpg', nombre: 'Ariel Vargas' },
+    { imagen: 'assets/jaren.jpg', nombre: 'Brian Mite' },
+  ];
 
   constructor() {}
 
