e148be0f6bda49a1d74cb8b2debdd5c27d3aa877
JarenPOL1015 <jarapazm@espol.edu.ec>
2024-12-26 15:44:35 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'pet_friendly',
+  webDir: 'www'
+};
+
+export default config;
