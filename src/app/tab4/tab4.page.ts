afc3c16d70002ed76db82b3b7221c0797dcfb325
JarenPOL1015 <jarapazm@espol.edu.ec>
2024-12-28 15:56:16 -0500

    Formulario funcional

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonIcon } from '@ionic/angular/standalone';
+import { FormsModule, ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonIcon, 
+  IonSelect, IonSelectOption, IonTextarea,IonButton } from '@ionic/angular/standalone';
 import { ProviderService } from '../services/provider.service';
 
 @Component({
@@ -9,14 +10,33 @@ import { ProviderService } from '../services/provider.service';
   templateUrl: './tab4.page.html',
   styleUrls: ['./tab4.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonIcon]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonIcon, 
+    IonSelect, IonSelectOption, IonTextarea,IonButton,
+    ReactiveFormsModule
+  ]
 })
 export class Tab4Page implements OnInit {
 
   collectionName = 'lugares_acogida';
+  collectionOpiniones = 'opiniones';
   dataList: { name: string; city: string; link: string; img: string }[] = [];
   filteredData: { name: string; city: string; link: string; img: string }[] = [];
   searchTerm: string = '';
+
+  /* Instanciando un form */
+  myForm: FormGroup = new FormGroup({
+    cantidad: new FormControl("", Validators.required),
+    ciudad: new FormControl("", Validators.required),
+    detalles: new FormControl("", Validators.required)
+  });
+
+  /* El método onSubmit para enviar los datos del formulario mediante el servicio  */
+
+  onSubmit() {
+    this.providerService.createDocument(this.collectionOpiniones, this.myForm.value).then(() => {
+        this.myForm.reset()
+    });
+  }
 
   constructor(private providerService: ProviderService) {}
 
