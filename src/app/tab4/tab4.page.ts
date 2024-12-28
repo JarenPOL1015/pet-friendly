14c06c9e215a5e323193057e959c82aea24ea86a
JarenPOL1015 <jarapazm@espol.edu.ec>
2024-12-28 16:19:44 -0500

    Formulario personalizado

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonIcon, 
-  IonSelect, IonSelectOption, IonTextarea,IonButton } from '@ionic/angular/standalone';
+  IonSelect, IonSelectOption, IonTextarea,IonButton, AlertController } from '@ionic/angular/standalone';
 import { ProviderService } from '../services/provider.service';
 
 @Component({
@@ -23,6 +23,16 @@ export class Tab4Page implements OnInit {
   filteredData: { name: string; city: string; link: string; img: string }[] = [];
   searchTerm: string = '';
 
+  // Lista de ciudades de Ecuador
+  cities: string[] = [
+    "Quito", "Guayaquil", "Cuenca", "Ambato", "Manta", "Portoviejo", 
+    "Loja", "Machala", "Esmeraldas", "Ibarra", "Riobamba", "Latacunga",
+    "Tena", "Tulcán", "Zamora", "Nueva Loja", "Babahoyo", "Milagro", 
+    "Quevedo", "Santo Domingo", "Durán", "Otro"
+  ];
+
+  formSubmitted: boolean = false;
+
   /* Instanciando un form */
   myForm: FormGroup = new FormGroup({
     cantidad: new FormControl("", Validators.required),
@@ -32,13 +42,22 @@ export class Tab4Page implements OnInit {
 
   /* El método onSubmit para enviar los datos del formulario mediante el servicio  */
 
-  onSubmit() {
-    this.providerService.createDocument(this.collectionOpiniones, this.myForm.value).then(() => {
-        this.myForm.reset()
+  async onSubmit() {
+    this.providerService.createDocument(this.collectionOpiniones, this.myForm.value).then(async () => {
+      this.formSubmitted = true;
+      this.myForm.reset();
+
+      const alert = await this.alertController.create({
+        header: 'Formulario Enviado',
+        message: '¡Gracias por enviar tu información! Pronto nos pondremos en contacto.',
+        buttons: ['OK'],
+      });
+
+      await alert.present();
     });
   }
 
-  constructor(private providerService: ProviderService) {}
+  constructor(private providerService: ProviderService, private alertController: AlertController) {}
 
   ngOnInit() {
     this.loadData();
