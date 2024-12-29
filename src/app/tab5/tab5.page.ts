5521c00aab8b3c01fc10cc9e152993be5d3a4321
Ariel-Vargas <arielvargas2004@hotmail.com>
2024-12-29 18:13:54 -0500

    carga de imagen y signals

--- src/app/tab5/tab5.page.ts
+++ src/app/tab5/tab5.page.ts
@@ -1,18 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, signal, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonFab, IonFabButton, IonIcon, IonCard,
+  IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+
+import { addIcons } from 'ionicons';
+import { cloudUploadOutline } from 'ionicons/icons';
+
 
 @Component({
   selector: 'app-tab5',
   templateUrl: './tab5.page.html',
   styleUrls: ['./tab5.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonFab, IonFabButton, IonIcon, IonCard,
+    IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class Tab5Page implements OnInit {
 
-  constructor() { }
+  imageReady = signal(false)
+  imageUrl = signal("")
+
+  constructor() { 
+    addIcons({ cloudUploadOutline });
+  }
+
+  onFileSelected(event: Event): void {
+    const input = event.target as HTMLInputElement;
+
+    if (input.files && input.files.length > 0) {
+      const file = input.files[0];
+      const reader = new FileReader();
+
+      // Convertir el archivo a una URL base64 para mostrarlo en el html
+      reader.onload = () => {
+      this.imageUrl.set(reader.result as string)
+      this.imageReady.set(true)
+      };
+
+      reader.readAsDataURL(file); // Leer el archivo como base64
+    }
+}
 
   ngOnInit() {
   }
